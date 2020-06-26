b46da59ce41a74fa2c201019f2aa277634a7fd0b
kjaa5 <kjaa5@LAPTOP-M84JJQD8>
2020-06-26 17:14:55 +0900

    test

---
+++ orange/src/ex/Main.java
@@ -0,0 +1,23 @@
+package ex;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		
+		primaryStage.setTitle("");
+		
+		primaryStage.show();
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
